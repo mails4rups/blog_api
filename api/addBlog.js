b4f1f3db0d1a4dbbe5932c3e5e07c455b33886e5
rupamjana <rupamjana@codomotive.com>
2019-09-11 12:59:11 +0530

    getting data for multipart formdata, add blog (unsettled)

--- api/addBlog.js
+++ api/addBlog.js
@@ -19,57 +19,62 @@ const path = require('path');
 let response={error:{},result:{}};
 
 /*** setting up storage engine for files & images ***/
-const imageStorage = multer.diskStorage({
-    destination : './storage/blogs/',
-    filename : function(req,file,cb){
-        cb(null,file.filename + '-' + Date.now() + path.extname(file.originalname));
-    }
-})
+ const imageStorage = multer.diskStorage({
+     destination : './storage/blogs/',
+     filename : function(req,file,cb){
+         cb(null,'blog' + '-' + Date.now() + path.extname(file.originalname));
+     }
+ });
 
-/*** defining upload function ***/
-const upload = multer({
-    storage : imageStorage
-}).single('blog-image');
+ /*** defining upload function ***/
+ const upload = multer({
+     storage : imageStorage
+ }).single('blogImage');
 
 
 /*** create blog using post ***/
 router.post('/',(req,res)=>{
 
-    const blogTitle = req.body.blogTitle;
-    const blogDesc = req.body.blogDesc;
-    const blogImage = req.body.blogImage;
-    const blogTimestamp = Date.now();
+    upload(req,res,err => {
+        const blogTitle = req.body.blogTitle;
+        const blogDesc = req.body.blogDesc;
+        const blogImage = req.body.blogImage;
+        const blogTimestamp = Date.now();
 
-    if(helpers.isHeaderValid(req.headers["content-type"],'multipart/form-data') == true){
         console.log(req.body)
-        if(
-            blogTitle!='' && blogTitle!=undefined &&
-            blogDesc!='' && blogDesc!=undefined 
-    
-        ){
-            /* db.getDb().collection(collection).insertOne(req.body,(err,result)=>{
-                if(err){
-                    response.error.error_data=1;
-                    response.error.error_msg=err;
+        console.log(req.file)
+        
+        if(helpers.isHeaderValid(req.headers["content-type"],'multipart/form-data') == true){
+            if(
+                blogTitle!='' && blogTitle!=undefined &&
+                blogDesc!='' && blogDesc!=undefined 
+        
+            ){
+                /* db.getDb().collection(collection).insertOne(req.body,(err,result)=>{
+                    if(err){
+                        response.error.error_data=1;
+                        response.error.error_msg=err;
+                        helpers.resultData(response,res);
+                    }else{
+                    // res.json({result : result,document : result.ops[0]}); 
+                    response.error.error_data=0;
+                    response.error.error_msg='';
+                    response.result=result.ops[0];
                     helpers.resultData(response,res);
-                }else{
-                   // res.json({result : result,document : result.ops[0]}); 
-                   response.error.error_data=0;
-                   response.error.error_msg='';
-                   response.result=result.ops[0];
-                   helpers.resultData(response,res);
-                }
-            }); */
+                    }
+                }); */
+            }else{
+                response.error.error_data=1;
+                response.error.error_msg='All fields must be field with values';
+                helpers.resultData(response,res);
+            }
         }else{
             response.error.error_data=1;
-            response.error.error_msg='All fields must be field with values';
+            response.error.error_msg='Input Type mismatched';
             helpers.resultData(response,res);
         }
-    }else{
-        response.error.error_data=1;
-        response.error.error_msg='Input Type mismatched';
-        helpers.resultData(response,res);
-    }
+    });
+    
     
     
 });
--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const bodyParser = require('body-parser');
 
 
 const app = express();
+
+/*** initializing body parser for JSON request ***/
 app.use(bodyParser.json());
+/*** initializing body parser for multipart formdata and others request ***/
+app.use(bodyParser.urlencoded({ extended : true}));
+
 const path = require('path');
 
 const db = require('./dbConfig');
@@ -13,18 +18,22 @@ module.exports = collection;
 
 
 
-
+/*** defining promise for database connection ***/
 
 db.connect((err)=>{
     if(err){
         console.log('Unable to connect to database');
         process.exit(1);
+        throw err;
     }else{
-        app.listen(3000,()=>{
-            console.log('Database connected successfully, app listening on port 3000');
-        });
+       console.log(' Database connected successfully ');
     }
 });
 
 app.use('/api/getBlogList',require('./api/getBlogList'));
 app.use('/api/addBlog',require('./api/addBlog'));
+
+/*** defining opening server ***/
+app.listen(3000,()=>{
+    console.log(' App listening on port 3000');
+});
